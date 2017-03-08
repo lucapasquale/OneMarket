aa945f81ae2994edba16acf276d03caaf652f4c2
Luca Pasquale <Luca Pasquale>
2017-03-08 16:06:10 -0300

    Adicionado postgresql

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 var Hapi = require('hapi');
+var pg = require('pg');
+const dbLoc = "postgres://postgres:2805@localhost:5432/carts";
 
-//Cria o server
+/*Cria o server*/
 var server = new Hapi.Server();
-server.connection({ port : 3000 });
+server.connection({ host: 'localhost', port : 3000 });
 
-//Adiciona routes
+/*Adiciona routes*/
 server.route(require('./routes/carts'));
+
+
+/*Instanteia o client do DB*/
+server.db = new pg.Client(dbLoc);
+server.db.connect(function (err){
+	if (err) throw err;
+});
+
+
+// server.db.query(`SELECT * FROM products WHERE id = $1`, [4], function(err, result){
+// 	if (err) throw err;
+
+// 	if (result.rows[0]) 
+// 		console.log(`id = ${result.rows[0].id} tem valor ${result.rows[0].name}`);
+// });
 
 //Inicia o server
 server.start(function() {
 	console.log('Running at ' + server.info.uri);
 });
+
