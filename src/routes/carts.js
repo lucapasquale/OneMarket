4d8212bdd1eafaed2329079628a5d7dc18317011
Luca Pasquale <Luca Pasquale>
2017-03-09 01:13:54 -0300

    Adicionado dependecies, adicionado POST, alterado para userId

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,4 +1,5 @@
 var pg = require('pg');
+var joi = require('joi');
 const dbLoc = "postgres://postgres:2805@localhost:5432/carts";
 
 /*Instanteia o client do DB*/
@@ -10,10 +11,10 @@ db.connect(function (err){
 /*METHODS*/
 module.exports = [
 
-	//Retorna todos os carts
+	//Retorna todos os produtos
   {
     method: 'GET',
-    path: '/carts',
+    path: '/products',
     handler: function(request, reply) {
 		console.log("Buscando todos os produtos");
 
@@ -30,42 +31,110 @@ module.exports = [
 	}
   },
 
-	//Dado um cartId, encontrar todos os produtos neste cart
+	//Dado um userId, encontrar todos os produtos no cart deste usuário
   {
     method: 'GET',
-    path: '/carts/{cartId}',
+    path: '/orders/{userId}',
     handler: function(request, reply) {
-    	const cartId = request.params.cartId;
-    	var resposta = { cartId : cartId, itens: [] };
+    	const userId = request.params.userId;
 
-		console.log(`Buscando carrinho com id: ${cartId}`);
-    	db.query(`SELECT quantity,productId FROM cartItems WHERE cartId = ${cartId}`, function(err, result){
+		console.log(`Buscando carrinho com id: ${userId}`);
+    	db.query(`SELECT productId,quantity FROM orders WHERE userId = ${userId}`, function(err, result){
     		if (err) throw err;
 
     		//Se não encontrou resultado retornar com erro
 			if (!result.rows[0]){
-				console.log('cartId ' + cartId + ' não existe. Retornando carrinho vazio');
-				return reply(resposta);
+				console.log('userId: ' + userId + ' não existe. Retornando carrinho vazio');
+				return reply({ userId : userId, products: [] });
 			}
 
-			resposta.itens = result.rows;
-			return reply(resposta);
+			return reply({ userId : userId, products: result.rows });
     	});
 	}
   },
 
-/*POST CART*/
+/*Insere o carrinho*/
   {
     method: 'POST',
+    path: '/orders',
+    config: {
+    	handler: function(request, reply) {
+    	var receivedCart = {
+    		userId: request.payload.userId,
+    		orders: request.payload.orders
+    	};
+
+		console.log(`Inserindo carrinho: ${JSON.stringify(receivedCart)}`);
+
+		//Para cada item do carrinho atual, inserir na tabela
+		for(i = 0; i < receivedCart.orders.length; i++){
+			//Obtem o pedido do array
+			var order = {productId: receivedCart.orders[i].productId, quantity: receivedCart.orders[i].quantity}
+
+			//Insere o pedido no DB na table orders
+			db.query(`INSERT INTO orders (userId, productId, quantity) 
+				VALUES (${receivedCart.userId}, ${order.productId}, ${order.quantity})`, function(err, result){
+					if (err) throw err;
+			});
+		};
+
+		//Retorna com o valor do userId
+    	return reply({userId: receivedCart.userId});
+		},
+
+		//Validação do payload
+		validate: {
+			payload: {
+				userId: joi.required(),
+
+				orders: joi.array().items(
+					joi.object().keys({
+						productId: joi.number().required(),
+						quantity: joi.number().required()
+					})
+				)
+			}
+		}
+    }
+    
+  },
+
+  {
+    method: 'PUT',
     path: '/carts',
     handler: function(request, reply) {
-    	console.log(request.payload);
-    	const cart = request.payload;
+    	const cartId = request.params.cartId;
+    	var cart = request.payload;
 
-    	console.log("Posted cart " + cart.id);
-    	values.push(cart);
+    	//Checa se existe o payload e se existem a propriedades necessárias
+    	if(!cart || !cart.cartId || !cart.products){
+    		console.log("Erro no payload: " + cart);
+    		return reply({ errorMessage: "Erro no payload: " + cart });
+    	};
 
- 		return reply('OK');
+		console.log(`Atualizando carrinho com id: ${cartId}`);
+
+		//Deleta carrinho antigo do cartId
+		db.query(`DELETE FROM cartItems 
+			WHERE cartId = ${cartId}`, function(err, result){
+			if (err) throw err;
+		});
+
+		//Para cada item do carrinho atual, inserir na tabela
+		console.log("Products: " + cart.products);
+		console.log("Products 0 : " + cart.products[0].keys);
+		for(p = 0; p < cart.products; p++){
+			var prod = cart.products[i];
+			console.log(i + ": " + prod);
+
+			db.query(`INSERT INTO cartItems (cartId, productId, quantity) 
+				VALUES (${cartId}, ${prod.productId}, ${prod.quantity})`, function(err, result){
+					if (err) throw err;
+			});
+		};
+
+		//Retorna com o valor do cartId
+    	return reply(cartId);
 	}
   }
 ];
